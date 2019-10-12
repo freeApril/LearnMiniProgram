66a44ca945916b7b965398eb6f10d311b0e876ad
an <742976739@qq.com>
2019-10-12 18:51:48 +0800

    小程序的三个初体验

--- page/home/home.js
+++ page/home/home.js
@@ -1,66 +1,25 @@
-// page/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'April',
+    age: 18,
+    star:[
+      {name: '周杰伦', skill: 'sing', age: '28'},
+      {name: '林俊杰', skill: 'sing', age: '18' },
+      {name: '成龙', skill: 'movie', age: '48' },
+      {name: '章子怡', skill: 'movie', age: '38' },
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  add(){
+    // console.log('发生了点击')
+    // this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  subs(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
